716d282ff09c9204754bae4069b0737a5c43d9c9
tail12 <tdallb12@gmail.com>
2016-04-01 10:53:49 +0800

    modelsにblog-phpの作成

---
+++ models/blog.php
@@ -0,0 +1,12 @@
+<?php
+    // M.Cファイルはクラス化する必要がある
+    // 命名規則はリソース名(テーブル名)の単数形
+    // 頭文字は大文字
+    class Blog{
+
+      public function index(){
+          echo "モデルファイルblog.phpのindex()メソッドが呼ばれました．";
+      }
+    }
+
+?>
