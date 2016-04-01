d0b557b3f93a941103a413ded8ac7d4010b0b485
tail12 <tdallb12@gmail.com>
2016-04-01 10:54:34 +0800

    コントローラの呼び出しを実装

--- routes.php
+++ routes.php
@@ -1,3 +1,28 @@
 <?php
-echo 'routes.phpを通りました';
+    echo 'routes.phpを通りました<br>';
+
+    $params = explode('/', $_GET['url']);
+    // 例：$params = explode('/', blogs/index)の場合↓
+    // http://192.168.33.11/seed_blog/blogs/index というリクエストが来た場合
+    // http://192.168.33.10/seed_blog/routes.php?url=blogs/index として処理する
+    // $_GETの中身
+    // $_GET['url']='blogs/index';
+    // $params = array['blogs', 'index']; ← $_GET['url']をexplodeすると，
+    // $params配列が完成する．
+
+    // パラメータの分解
+    $resource = $params[0]; // 上記リクエストの場合はblogが格納される
+    $action = $params[1];   //
+    $id = 0;
+
+    // オプションになにか入っていれば…
+    if (isset($params[2])) {
+      $id = $params[2];
+    }
+
+    // コントローラの呼び出し
+    // 作成した変数を文字列連結させ，require()を使って，必要なコントローラーファイルを呼び出す
+    echo 'controllers/' . $resource . '_controller.php<br>';
+    require('controllers/'. $resource . '_controller.php');
+
 ?>
