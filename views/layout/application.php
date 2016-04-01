557fef586606305a27a48eb577f28e1e54102eea
tail12 <tdallb12@gmail.com>
2016-04-01 11:24:19 +0800

    viewの追加

---
+++ views/layout/application.php
@@ -0,0 +1,11 @@
+<!-- 表示したいページによりデザインデータが違う -->
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>フレームワーク ブログ</title>
+  </head>
+  <body>
+    <!-- 各ページに必要なコードをURLを元にrequire -->
+  </body>
+</html>
