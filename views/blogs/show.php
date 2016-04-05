0eddde8bc551422984b65b3143d63876df4ee46b
tail12 <tdallb12@gmail.com>
2016-04-05 09:07:59 +0800

    個ページの作成

---
+++ views/blogs/show.php
@@ -0,0 +1,6 @@
+<h1>
+  <span style="color: red;"><?php echo $this->viewOption['id']; ?></span>
+  <?php echo $this->viewOption['title']; ?>
+</h1>
+
+<p><?php echo $this->viewOption['body']; ?></p>
